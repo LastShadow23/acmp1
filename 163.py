19938cd0f041dae461bf534c7b6a99e6fb7a6042
Ruslan <rhafizov01@gmail.com>
2022-03-23 23:50:19 +0300

    new

--- 163.py
+++ 163.py
@@ -4,7 +4,7 @@ if eq[0] == 'x':
     print(int(eq[4]) - int(eq[1] + eq[2]))
 
 elif eq[2] == 'x':
-    print((int(eq[4]) - int(eq[0])) * (int(eq[1]+'1')))
+    print((int(eq[4]) - int(eq[0])) * int(eq[1]+'1'))
 
 else:
     print(int(eq[0]) + int(eq[1] + eq[2]))
