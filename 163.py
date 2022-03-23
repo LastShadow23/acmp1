33cbee1b94d34ee4565289ed10270523648c7cd4
Ruslan <rhafizov01@gmail.com>
2022-03-23 23:51:01 +0300

    new

--- 163.py
+++ 163.py
@@ -1,10 +1,10 @@
-eq = input()
+eq = input()  # уравнение
 
 if eq[0] == 'x':
     print(int(eq[4]) - int(eq[1] + eq[2]))
 
 elif eq[2] == 'x':
-    print((int(eq[4]) - int(eq[0])) * int(eq[1]+'1'))
+    print((int(eq[4]) - int(eq[0])) * int(eq[1] + '1'))
 
 else:
     print(int(eq[0]) + int(eq[1] + eq[2]))
