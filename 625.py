fe2d7b472282988d9674a9ec1cae4505d0bc5a83
Ruslan <rhafizov01@gmail.com>
2022-03-23 20:39:02 +0300

    new

--- 625.py
+++ 625.py
@@ -1,8 +1,8 @@
 keyboard = {}
 
 for key in range(1, 10):
-    for click, letter in enumerate(input(), 1):
-        keyboard[letter] = {"click": click, "key": key}
+    for click, char in enumerate(input(), 1):
+        keyboard[char] = {"click": click, "key": key}
 
 msg = input()
 
@@ -17,17 +17,16 @@ for a in msg:
         key_num_old = 10
         continue
 
-    if keyboard.get(a):
-        if keyboard[a]['key'] == 1:
-            click_num = keyboard[a]['click']
-            key_num = keyboard[a]['key']
-            if key_num_old == key_num:
-                press += 1
-            key_num_old = key_num
-            press += click_num
-            if not up_index:
-                temp_up_index = True
-            continue
+    if a in ('.', '!', '?'):
+        click_num = keyboard[a]['click']
+        key_num = keyboard[a]['key']
+        if key_num_old == key_num:
+            press += 1
+        key_num_old = key_num
+        press += click_num
+        if not up_index:
+            temp_up_index = True
+        continue
 
     if not keyboard.get(a):
         if temp_up_index:
