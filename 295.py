b3ebf2053cf2b5b18d4baedd2974233799f18577
Ruslan <rhafizov01@gmail.com>
2022-03-25 14:22:15 +0300

    new

--- 295.py
+++ 295.py
@@ -1,33 +1,34 @@
 msg = input()
 msg_in = input()
-n = 30
+MAX_SHIFT = 26
+MAX_ABC = 90
+MIN_ABC = 64
+shift = 30
 help = ''
 
+for offset in range(MAX_SHIFT):
 
-for i in range(26):
-
-    for j in range(len(msg_in)):
-        if ord(msg_in[j]) + i > 90:
-            help += chr(64 + (ord(msg_in[j])+i) % 90)
+    for char in msg_in:
+        if ord(char) + offset > MAX_ABC:
+            help += chr(MIN_ABC + (ord(char) + offset) % MAX_ABC)
         else:
-            help += chr(ord(msg_in[j]) + i)
+            help += chr(ord(char) + offset)
 
     if msg.find(help) != -1 or msg == help:
-        n = i
+        shift = offset
         break
 
     help = ''
 
-
-if n == 30:
+if shift == 30:
     print('IMPOSSIBLE')
 else:
     help = ''
 
-    for i in range(len(msg)):
-        if ord(msg[i]) - n < 65:
-            help += chr(90-(64-(ord(msg[i])-n)))
+    for char in msg:
+        if ord(char) - shift <= MIN_ABC:
+            help += chr(MAX_ABC - (MIN_ABC - (ord(char) - shift)))
         else:
-            help += chr(ord(msg[i])-n)
+            help += chr(ord(char) - shift)
 
     print(help)
